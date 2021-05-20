de2706ecc7106315bb867e10dd77969ee2a253a7
Shabdan <shabdan.abjaparov,official@gmail.com>
2021-05-20 16:04:23 +0600

    fs

--- src/main/java/com/project/coffee/service/OrderServiceImpl.java
+++ src/main/java/com/project/coffee/service/OrderServiceImpl.java
@@ -1,43 +1,43 @@
-package com.project.coffee.service;
-
-import com.project.coffee.model.Order;
-import com.project.coffee.repository.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.util.HashSet;
-import java.util.Set;
-
-@Service
-public class OrderServiceImpl implements OrderService{
-    @Autowired
-    OrderRepository orderRepository;
-
-    @Override
-    public Set<Order> getOrders() {
-        return new HashSet<>(orderRepository.findAll());
-    }
-
-    @Override
-    public Order getOrderById(Long id) {
-        return orderRepository.findById(id).get();
-        //SELECT id, items FROM TABLE WHERE ID = id --> Query
-    }
-
-    @Override
-    public Order create(Order order) {
-        return orderRepository.save(order);
-        //INSERT INTO TABLE(column1, column2,...) VALUES(value1, value2,..)
-    }
-
-    @Override
-    public Order update(Order order) {
-        return orderRepository.save(order);
-        //UPDATE TABLE SET total = 100, .. WHERE ID = id
-    }
-
-    @Override
-    public void delete(Long id) {
-        orderRepository.deleteById(id);
-        //DELETE FROM TABLE WHERE ID = id
-    }
-}
+//package com.project.coffee.service;
+//
+//import com.project.coffee.model.Order;
+//import com.project.coffee.repository.OrderRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//@Service
+//public class OrderServiceImpl implements OrderService{
+//    @Autowired
+//    OrderRepository orderRepository;
+//
+//    @Override
+//    public Set<Order> getOrders() {
+//        return new HashSet<>(orderRepository.findAll());
+//    }
+//
+//    @Override
+//    public Order getOrderById(Long id) {
+//        return orderRepository.findById(id).get();
+//        //SELECT id, items FROM TABLE WHERE ID = id --> Query
+//    }
+//
+//    @Override
+//    public Order create(Order order) {
+//        return orderRepository.save(order);
+//        //INSERT INTO TABLE(column1, column2,...) VALUES(value1, value2,..)
+//    }
+//
+//    @Override
+//    public Order update(Order order) {
+//        return orderRepository.save(order);
+//        //UPDATE TABLE SET total = 100, .. WHERE ID = id
+//    }
+//
+//    @Override
+//    public void delete(Long id) {
+//        orderRepository.deleteById(id);
+//        //DELETE FROM TABLE WHERE ID = id
+//    }
+//}
