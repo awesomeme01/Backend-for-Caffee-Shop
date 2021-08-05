285b334576430559e3629ef326a2aefdce840267
Shabdan <shabdan.abjaparov,official@gmail.com>
2021-08-06 00:42:42 +0600

    fixed findByName query

--- src/main/java/com/project/coffee/repository/CategoryRepository.java
+++ src/main/java/com/project/coffee/repository/CategoryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository <Category, Long> {
-    @Query("SELECT c FROM Category c WHERE LOWER(u.name) = LOWER(:name)")
+    @Query("SELECT c FROM Category c WHERE LOWER(c.name) = LOWER(:name)")
     Category findByName(@Param("name") String name);
 }
